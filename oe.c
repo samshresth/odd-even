20947ef759fbd5c08914aec2c240928e96e851f8
Samyam Shresth <samyam39shrestha39@gmail.com>
2021-05-16 04:28:35 +0545

    This is c program

---
+++ oe.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+manin()
+{
+    printf("Enter any integer number");
+    scanf("%d", n);
+    if (n%2=o)
+    printf("The number is even");
+    else 
+    printf("The number is odd");
+    
+}
